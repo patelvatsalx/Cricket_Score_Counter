e2698bfc8f9da2a0f4b640038eb898c71ce9a8cc
Vatsal <patelvatsal2305@gmail.com>
2025-06-14 10:32:57 +0530

    Button name changed

--- src/Runs.jsx
+++ src/Runs.jsx
@@ -7,8 +7,8 @@ function Runs() {
             <div className="container">
                 <h3 className="counter">Runs: {runs}</h3>
                 <div className="button-group">
-                    <button className="btn" onClick={() => setRuns(runs + 1)}>Increment</button>
-                    <button className="btn" onClick={() => setRuns(runs > 0 ? runs - 1 : 0)}>Decrement</button>
+                    <button className="btn" onClick={() => setRuns(runs + 1)}>Increase</button>
+                    <button className="btn" onClick={() => setRuns(runs > 0 ? runs - 1 : 0)}>Decrease</button>
                 </div>
             </div>
         </div>
